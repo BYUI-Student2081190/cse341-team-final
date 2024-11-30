58afabe5e9bbe8b9a804a1753026c3a26f4eec30
byui_student1045 <sco22022@byui.edu>
2024-11-30 08:22:37 -0700

    Updated the index route so it links to all the other routes.

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,8 +4,14 @@ const router = express.Router();
 
 
 /** Routes **/
+// This intro can get changed later on
 router.get('/', (req, res) => {res.json( {message: 'Hello! Welcome to the api! You are running and are connected to the db!'} )});
-// Add more routes to access the other route files from the index
+// Add a route to swagger documentation later...
+// Routes to all the other routers
+router.use('/books', require('./bookRoute'));
+router.use('/movies', require('./movieRoute'));
+router.use('/videogames', require('./videogameRoute'));
+router.use('/users', require('./userRoute'));
 
 
 
