89994dc901990012b43d03e72aff8762b5050a4f
byui_student1045 <sco22022@byui.edu>
2024-12-12 08:05:29 -0700

    Updated test-response to work with all collections

--- utilities/test-response.js
+++ utilities/test-response.js
@@ -6,7 +6,17 @@ module.exports = class TestResponse {
     }
 
     data = {}
+    // Why we do this is so when we run
+    // res.status.json or res.status.send
+    // whatever was going to be sent to the
+    // user instead gets saved in this res
+    // varible as data that we can use to
+    // test with.
     json(data) {
+        this.data = data
+    }
+
+    send(data) {
         this.data = data
     }
 
