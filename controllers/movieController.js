1b372b39778d4f142caddb7c7838223cce21aaaa
byui_student1045 <sco22022@byui.edu>
2024-12-11 21:41:47 -0700

    Unit Tests added

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -45,7 +45,7 @@ const getSingleMovieById = async (req, res) => {
         if (!movie) {
             return res.status(404).json({ error: 'Movie not found by ID' });
         }
-
+        res.data = movie;
         res.status(200).json(movie);
     } catch (error) {
         console.error('Error fetching movie by ID:', error.message);
