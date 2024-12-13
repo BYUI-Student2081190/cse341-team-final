09971b425789f708373c9bc300ba90a1f1b854db
byui_student1045 <sco22022@byui.edu>
2024-12-12 23:44:40 -0700

    Created a way for dev to happen with api/docs and for production to have it

--- routes/swaggerRoute.js
+++ routes/swaggerRoute.js
@@ -2,10 +2,15 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../swagger.json');
+const productionSwaggerDoc = require('../swagger_production.json');
 
 /** Routes **/
 router.use('/', swaggerUi.serve);
-router.use('/', swaggerUi.setup(swaggerDoc));
+if (!process.env.PORT) {
+    router.use('/', swaggerUi.setup(swaggerDoc));
+} else {
+    router.use('/', swaggerUi.setup(productionSwaggerDoc));
+}
 
 /** Exports **/
 module.exports = router;
